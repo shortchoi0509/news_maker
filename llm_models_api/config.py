568f1740220a2fcf9c8d915bc488a72b7bbe2338
shortchoi0509 <71883353+shortchoi0509@users.noreply.github.com>
2026-05-06 18:08:37 +0900

    Delete llm_models_api/config.py

--- llm_models_api/config.py
+++
@@ -1,5 +0,0 @@
-def deepseek_api_key():
-    return "sk-e82033a9040b48adac0e0ffda498e588"
-
-def chatgpt_api_key():
-    return "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"
